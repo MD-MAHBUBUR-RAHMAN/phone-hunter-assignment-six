a7816552e20c4eb5b2919b994004d4dd8e7f933f
MD-MAHBUBUR-RAHMAN <nirjhor211288@gmapl.com>
2022-03-01 03:26:08 +0600

    Add single Item detail

--- js/phone.js
+++ js/phone.js
@@ -4,28 +4,32 @@ const serchText = searchField.value
 
 const url = `https://openapi.programming-hero.com/api/phones?search=${serchText}`
 fetch(url)
-.then(res => res.json())
-.then(data => displayResult(data.data))
-
-    searchField.value = ''
+    .then(res => res.json())
+    .then(data => displayResult(data.data))
+//clear input field:
+searchField.value = ''
 }
-const displayResult = (phones) => {
-    // // //get container from Html file where result will  be displaied:---------
+
+const displayResult = phones => {
+    ////------:get container from Html file:---------
     const searchResult = document.getElementById('search-result')
-    // // //find all elements in arreay using forEach loop:---------
+
+    // clear div at new search:---
+    searchResult.innerText = ''
+    // // find all elements in arreay using forEach loop:---------
     phones.forEach(phone => {
         // console.log(phone) 
         // // // Create Div and add class: ----------
         const div = document.createElement('div')
         div.classList.add("col")
         div.innerHTML = `
-        <div class="card h-100">
-            <img src="${phone.image}" class="card-img-top " alt="...">
+        <div class="card h-100">            
+            <img src="${phone.image}" class="card-img-top" alt="...">   
             <div class="card-body">
                 <h5 class="card-title">${phone.phone_name}</h5>
                 <h5 class="card-title">${phone.brand}</h5>
-                <p class="card-text"></p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <h5 class="card-title">${phone.slug}</h5>
+                <a onClick="displayDetail('${phone.slug}')" href="#" class="btn btn-primary">Detail</a>
             </div>
         </div>
         `                   
@@ -34,4 +38,37 @@ const displayResult = (phones) => {
     })
 }
 
-// onClick="phoneDetail(${phone})"
+// phone detail
+const displayDetail = (detail) => {
+    const url =`https://openapi.programming-hero.com/api/phone/${detail}`
+    fetch(url)
+    // console.log(url)
+    .then(res => res.json())
+    .then(data =>  singleItem(data))
+
+    const singleItem = itemDetail=>{
+        // console.log(itemDetail.data)
+
+    const detailDiv = document.getElementById('detail')
+    //clear detail
+    detailDiv.innerText = ''
+    const div = document.createElement('div')
+    div.classList.add("card")
+        div.innerHTML = `
+            <div class="card h-100">            
+                <img src="${itemDetail.data.image}" class="card-img-top" alt="...">   
+                <div class="card-body">
+                    <h5 class="card-title">${itemDetail.data.name}</h5>
+                    <h5 class="card-title">${itemDetail.data.brand}</h5>
+                    <h5 class="card-title">${itemDetail.data.releaseDate}</h5>
+                </div>
+            </div>
+            ` 
+    detailDiv.appendChild(div) 
+    }
+
+
+
+
+   
+}
